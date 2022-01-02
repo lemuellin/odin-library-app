eb26303ec13224179fbe4a6714336fda5e0ed979
lemuellin <lemuellin@gmail.com>
2022-01-01 20:15:53 -0800

    first version

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # odin-library-app
+
+
+<a href='https://www.freepik.com/photos/background'>Background photo created by jcomp - www.freepik.com</a>
---
+++ script.js
@@ -0,0 +1,87 @@
+let myLibrary = [];
+
+const book1 = new book('Digital Minimalism','Cal Newport','500','read');
+const book2 = new book('Atomic Habits','James Clear','150','not read');
+const book3 = new book('Rich Dad Poor Dad','Robert T.Kiyosaki','500','read');
+myLibrary.push(book1,book2,book3);
+
+const addBook = document.querySelector('.addBook');
+addBook.addEventListener('click', ()=>addBookToLibrary());
+
+
+function book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`);
+    }
+}
+
+function addBookToLibrary(){
+    let title = document.querySelector('.title').value;
+    let author = document.querySelector('.author').value;
+    let pages = document.querySelector('.pages').value;
+    let read = document.querySelector('.read').value;
+    console.log(read);
+    
+    let bookNew = new book(title, author, pages, read);
+    myLibrary.push(bookNew);
+    displayData();
+    closeForm();
+}
+
+
+function displayData(){
+    let tables ="";
+
+    for (eachBook in myLibrary){
+        tables += 
+        "<td>"+ myLibrary[eachBook].title +"</td>" +
+        "<td>"+ myLibrary[eachBook].author +"</td>" +
+        "<td>"+ myLibrary[eachBook].pages +"</td>" +
+        "<td>"+ `<button class='changeRead' onclick="changeRead(${eachBook})">`+
+        myLibrary[eachBook].read +"</button>" +
+        `<button class='rmvBook' onclick="rmvBook(${eachBook})">Remove</button>`+
+        "</td>" +
+        "</tr>";
+    }
+
+    document.querySelector(".table").innerHTML = 
+        "<thead>"+"<tr>" +
+        "<th>"+"Title"+"</th>" +
+        "<th>"+"Author"+"</th>" +
+        "<th>"+"Pages"+"</th>" +
+        "<th>"+"Status"+"</th>" +
+        "</tr>" + "</thead>" +
+        "<tbody>"+"<tr>" +
+        tables+"</tbody>";
+}
+displayData();
+
+function changeRead(a){
+    console.log(myLibrary[a].read);
+    //myLibrary[a].read == "read"? "not read":"read";
+    if (myLibrary[a].read == 'read') {
+        myLibrary[a].read = 'not read';
+      } else {
+        myLibrary[a].read = 'read';
+      }
+    console.log(myLibrary[a].read);
+    displayData();
+}
+
+function rmvBook(a){
+    myLibrary.splice(a,1);
+    displayData();
+}
+
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
